fe53f679244f72b218a43581de667e6a4d899eaa
Peter Baehr <pete.baehr@gmail.com>
2013-09-13 14:03:56 -0400

    Condense common xor code.

--- otp.py
+++ otp.py
@@ -1,16 +1,19 @@
 from os import urandom
 
 
+def xor_bytearray(ba1, ba2):
+    c = bytearray()
+    for a, b in zip(ba1, ba2):
+        c.append(a ^ b)
+    return c
+
+
 def otp_encode(plaintext):
     key = bytearray(urandom(len(plaintext)))
-    ciphertext = bytearray()
-    for a, b in zip(bytearray(plaintext), bytearray(key)):
-        ciphertext.append(a ^ b)
+    ciphertext = xor_bytearray(bytearray(plaintext), key)
     return ciphertext, key
 
 
 def otp_decode(ciphertext, key):
-    plaintext = bytearray()
-    for a, b in zip(bytearray(ciphertext), bytearray(key)):
-        plaintext.append(a ^ b)
+    plaintext = xor_bytearray(bytearray(ciphertext), bytearray(key))
     return plaintext
